4499601fd7ae9f47048604c04018a3440f87c684
Romain Durade <durade.romain@gmail.com>
2022-10-13 15:27:57 +0200

    add podman

--- aliases/bash.d/softwares.sh
+++ aliases/bash.d/softwares.sh
@@ -4,7 +4,13 @@ if hash codium 2>/dev/null; then
   alias code=codium
 fi
 
+if hash podman 2>/dev/null; then
+  alias docker=podman
+  if hash docker-compose 2>/dev/null; then
+    export DOCKER_HOST=unix:///run/user/$UID/podman/podman.sock
+  fi
+fi
+
 if hash yarn 2>/dev/null; then 
   export PATH="$PATH:`yarn global bin`"
 fi
-
